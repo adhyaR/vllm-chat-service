125c443230e25f8450211a37593bbcdc10e22ed0
Adhya <adhya.rajaram20@imperial.ac.uk>
2025-09-30 11:02:27 +0100

    Added a health check for vLLM Chat Service

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from vllm_chat_service.api.main import app
+
+
+@pytest_asyncio.fixture
+async def async_client():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as async_client:
+        yield async_client
